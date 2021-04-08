4f221e3790473d3ca99d62aa466fea7876f10edf
eatmorepies-dev <alexandermsummers@gmail.com>
2021-04-07 23:54:28 -0400

    Revised comment for .eslintrc.js in WEEK12

--- WEEK12/.eslintrc.js
+++ WEEK12/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = { 
     rules: {
-        'no-console': 'off', //Used to enable console log in the script
+        'no-console': 'off', //Used to enable console.log in the editor
     },
 };
